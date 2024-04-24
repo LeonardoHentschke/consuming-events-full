0d80e00993c3b39120ae2c0e81daa5fa99e9eb40
Leonardo Hentschke <leonardo.hentschke@bimachine.com.br>
2024-04-24 17:23:18 -0300

    edit

--- cons-subscript/server.ts
+++ cons-subscript/server.ts
@@ -1,6 +1,6 @@
-import fastify, { FastifyReply } from "fastify";
+import fastify, {FastifyReply} from "fastify";
 import axios from "axios";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
+import {ZodTypeProvider} from "fastify-type-provider-zod";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -11,20 +11,50 @@ const authHeader = () => ({
 });
 
 const errorHandler = (reply: FastifyReply, errorMessage: string) => {
-    reply.status(500).send({ error: errorMessage });
+    reply.status(500).send({error: errorMessage});
 };
 
 app.get('/consuming/subscriptions/:userId', async (req, reply) => {
     try {
         // @ts-ignore
         const userId = req.params.userId;
-        const { data } = await axios.get(`http://localhost:3333/subscriptions/${userId}`, authHeader());
+        const {data} = await axios.get(`http://localhost:3333/subscriptions/${userId}`, authHeader());
         reply.send(data);
     } catch (error) {
         errorHandler(reply, 'Erro ao consumir o endpoint /consuming/subscriptions/:userId');
     }
 });
 
-app.listen({ port: 5555, host: '0.0.0.0' }).then(() => {
+app.post('/consuming/subscribe', async (req, reply) => {
+    try {
+        // @ts-ignore
+        const {userId, eventId} = req.body;
+        const {data} = await axios.post('http://localhost:3333/subscriptions', {userId, eventId}, authHeader());
+        reply.send(data);
+    } catch (error) {
+        errorHandler(reply, 'Erro ao consumir o endpoint /consuming/subscribe');
+    }
+});
+
+app.delete('/consuming/subscription', async (req, reply) => {
+    try {
+        // @ts-ignore
+        const { userId, eventId } = req.body;
+        const config = {
+            ...authHeader(),
+            params: {
+                userId,
+                eventId
+            }
+        };
+        const { data } = await axios.delete('http://localhost:3333/subscription', config);
+        reply.send(data);
+    } catch (error) {
+        errorHandler(reply, 'Erro ao consumir o endpoint /consuming/subscription');
+    }
+});
+
+
+app.listen({port: 5555, host: '0.0.0.0'}).then(() => {
     console.log('Server para consumir subscriptions de Events Full - running!')
 });
